d0e96a2e931ddb8b385493969e804ff9682adf19
SethTan1 <111527639+SethTan1@users.noreply.github.com>
2022-08-23 12:38:54 -0500

    Started Bluej and Chapter 1 notes

---
+++ My First BlueJ project/Chapter1Notes.java
@@ -0,0 +1,46 @@
+
+/**
+ * Seth Tan
+ * 8/23/22
+ * Notes for Chapter 1
+ * where we are going to take some code notes
+ * Who helped me:
+ * 
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import java.util.Scanner;
+import java.util.Random;
+
+
+public class Chapter1Notes
+{
+    
+    
+    /**
+     * main method
+     * where we are going to code
+     */
+    public static void main(String[] args)
+    {
+     
+         Scanner keyboard = new Scanner (System.in);   
+         
+         //these are the basic data types
+         int x = 9, y, z;
+         double num = 5, num1 = 23.4567, num2;
+         char letter = 'd';
+         boolean choice = false;
+         float anum = 7;
+         long xy = 9;
+         short b = 5;
+         
+         //these are objects
+         String word = new String("Seth Tan");
+         Integer number = new Integer(47);
+         Double number2 = new Double(34.5);
+         
+         
+         
+    }
+}
